be8ec6fe4d3e696563848c3aff19b58b57752551
Venuka Onadith <onadhith@gmail.com>
2026-07-15 14:16:31 +0530

    feat: add projects section with status badges and GitHub links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
+      <Projects />
+
+      {/* Temporary placeholder */}
       <div className="h-64 bg-slate flex items-center justify-center">
         <p className="text-muted font-body">More sections coming soon...</p>
       </div>
